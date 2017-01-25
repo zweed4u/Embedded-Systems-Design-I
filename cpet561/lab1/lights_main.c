2f980748fe772a877af023e891a1aaaaf25d2f1f
Zachary Weeden <zdw7287@rit.edu>
2017-01-25 02:42:56 -0500

    Added comments for better understanding?

--- cpet561/lab1/lights_main.c
+++ cpet561/lab1/lights_main.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-	unsigned char *switchesBase_ptr = (unsigned char *) SWITCHES_BASE;
-	unsigned char *ledsBase_ptr = (unsigned char *) LEDS_BASE;
-	unsigned char switch_val;
+	unsigned char *switchesBase_ptr = (unsigned char *) SWITCHES_BASE;	//create & declare a pointer to casted variable found in system.h - address = switches_base
+	unsigned char *ledsBase_ptr = (unsigned char *) LEDS_BASE;			//create & declare a pointer to casted variable found in system.h - address = leds_base
+	unsigned char switch_val;											//create 1 byte variable
 
 	while (1) {
-		switch_val = *switchesBase_ptr;
-		*ledsBase_ptr = switch_val;
+		switch_val = *switchesBase_ptr;		//switch_val gets data of the address where switchesBase_ptr points to 
+		*ledsBase_ptr = switch_val;			//set the data to switch_val for the address where ledsBase_ptr points to
 	}
 }
