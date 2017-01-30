6bd1f9e314b4eebd35ef5396fa2233bfa6d528b7
Zachary Weeden (RIT Student) <zdw7287@ad.rit.edu>
2017-01-30 15:08:19 -0500

    added instruction ref doc and comments to assembly src

--- cpet561/refCode/example_main.s
+++ cpet561/refCode/example_main.s
@@ -13,7 +13,7 @@
   ori \reg, \reg, %lo(\addr)
 .endm
 
-# define constants
+# define constants - address of i/o for these peripherals
 .equ Switches, 0x00011000
 .equ LEDs,     0x00011010
 
@@ -23,7 +23,7 @@ main: 	#load r2 and r3 with the addresses
   movia r2, Switches
   movia r3, LEDs
 
-loop: 	#read from r2 and store to r3
+loop: 	#read from r2 and store to r3 - pointer-esque () denotes use of the contents as pointer, go to that address in mem annd use those contents, 0 is an offest how many address to jump after lookup of r2 number
   ldbio r4, 0(r2)
   stbio r4, 0(r3)
   br    loop
