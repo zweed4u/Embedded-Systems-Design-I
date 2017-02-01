0058c13782fc690596f9a9ab7bf19ef03c128521
Zachary Weeden (RIT Student) <zdw7287@ad.rit.edu>
2017-02-01 14:40:49 -0500

    Nios processor created in qsys

---
+++ cpet561/lab2/nios_system/nios_system_inst.v
@@ -0,0 +1,8 @@
+	nios_system u0 (
+		.clk_clk              (<connected-to-clk_clk>),              //           clk.clk
+		.reset_reset_n        (<connected-to-reset_reset_n>),        //         reset.reset_n
+		.i_switch_export      (<connected-to-i_switch_export>),      //      i_switch.export
+		.i_trigger_export     (<connected-to-i_trigger_export>),     //     i_trigger.export
+		.o_accumulator_export (<connected-to-o_accumulator_export>)  // o_accumulator.export
+	);
+
---
+++ cpet561/lab2/nios_system/nios_system_inst.vhd
@@ -0,0 +1,19 @@
+	component nios_system is
+		port (
+			clk_clk              : in  std_logic                     := 'X';             -- clk
+			reset_reset_n        : in  std_logic                     := 'X';             -- reset_n
+			i_switch_export      : in  std_logic_vector(7 downto 0)  := (others => 'X'); -- export
+			i_trigger_export     : in  std_logic                     := 'X';             -- export
+			o_accumulator_export : out std_logic_vector(15 downto 0)                     -- export
+		);
+	end component nios_system;
+
+	u0 : component nios_system
+		port map (
+			clk_clk              => CONNECTED_TO_clk_clk,              --           clk.clk
+			reset_reset_n        => CONNECTED_TO_reset_reset_n,        --         reset.reset_n
+			i_switch_export      => CONNECTED_TO_i_switch_export,      --      i_switch.export
+			i_trigger_export     => CONNECTED_TO_i_trigger_export,     --     i_trigger.export
+			o_accumulator_export => CONNECTED_TO_o_accumulator_export  -- o_accumulator.export
+		);
+
