e7f44198280f8448f9121ae6fc27acdb917eb5e6
Zachary Weeden (RIT Student) <zdw7287@ad.rit.edu>
2017-02-08 15:30:40 -0500

    Assembly demoed - C interrupts

---
+++ cpet561/lab2/software/nios_software/interrupt_main.c
@@ -0,0 +1,103 @@
+#include <sys/alt_irq.h>                // for IRQ support function
+#include "alt_types.h"                  // for standard embedded types
+#include "system.h"                     // for Qsys #defines
+
+#define PIO_DATA_REG_OFFSET         0
+#define PIO_DATA_DIR_REG_OFFSET     1
+#define PIO_INT_MASK_REG_OFFSET     2
+#define PIO_EDGE_CAP_REG_OFFSET     3
+
+#define KEY1_BIT_MASK               0x1         // bit 0
+#define KEY2_BIT_MASK               0x2         // bit 1
+#define KEY3_BIT_MASK               0x4         // bit 2
+
+uint32* switchesPtr = (uint32*)I_SWITCH_BASE; // may want or need volatile
+uint32* triggerPtr  = (uint32*)I_TRIGGER_BASE
+uint32* accumulatorPtr  = (uint32*)O_ACCUMULATOR_BASE
+//unsigned long accumulator_val;
+/*
+uint32* grnLedsPtr  = (uint32*)GRN_LEDS_BASE;
+uint32* redLedsPtr  = (uint32*)RED_LEDS_BASE;
+uint32* bcdPortPtr  = (uint32*)BCD_NUMBER_BASE;
+*/
+static uint32 sRunningSum  = 0;
+static uint8  sSwitchValue = 0;
+
+static uint32 sKey1Pressed = FALSE;
+
+volatile uint32* pushButtonPtr = (uint32*)(PUSHBUTTON_BASE);
+
+void pushButtonIsr(void *context)
+{
+  uint32 reg_value   = 0;
+  uint32 reg_value   = 0;
+
+  // reset key pressed flags
+  sKey1Pressed = FALSE;
+
+
+  // read the Edge Capture register
+  reg_value = *(pushbuttonsPtr + PIO_EDGE_CAP_REG_OFFSET);
+
+  // determine which push button was pressed
+  // assume more than one could be pressed at the same time
+  if (KEY1_BIT_MASK == (reg_value & KEY1_BIT_MASK))
+  {
+    // read the switches
+    sSwitchValue = (uint8)*switchesPtr;
+
+    sKey1Pressed = TRUE;
+  } /* if */
+
+  // clear the interrupt bits set by writing back value read
+  *(pushbuttonsPtr + PIO_EDGE_CAP_REG_OFFSET) = reg_value;
+
+} /* pushButtonIsr */
+
+int main(void)
+{
+
+  // Turn all LEDS off
+  *accumulatorPtr = 0;
+
+  // register the Interrupt Service Handler
+  alt_ic_isr_register(PUSHBUTTON_IRQ_INTERRUPT_CONTROLLER_ID, PUSHBUTTON_IRQ,
+                      pushButtonIsr, 0, 0);
+
+  // ------------------------------------------------------------------
+  // clear edge capture register of pushButtonPtr and then unmask
+  // interrupt for KEY1_BIT_MASK
+  // ------------------------------------------------------------------
+  *(pushButtonPtr + PIO_EDGE_CAP_REG_OFFSET) = (KEY1_BIT_MASK);
+  *(pushButtonPtr + PIO_INT_MASK_REG_OFFSET) = (KEY1_BIT_MASK);
+
+  // loop forever
+  while (1)
+  {
+    if (sKey1Pressed)
+    {
+      // update running sum & grn LEDs
+      sRunningSum += sSwitchValue;
+      *(grnLedsPtr + PIO_DATA_REG_OFFSET) = sSwitchValue;
+
+      sKey1Pressed = FALSE;
+    } /* if KEY1_BIT_MASK pressed */
+    else
+    {
+      // update the green leds with switch value
+      *(grnLedsPtr + PIO_DATA_REG_OFFSET) = *(switchesPtr + PIO_DATA_REG_OFFSET);
+    } /* else */
+
+    // output sum to red LEDs
+    *(redLedsPtr + PIO_DATA_REG_OFFSET)  = sRunningSum;
+
+    // convert sum to bcd number
+    bcd_value = HexToBcd(sRunningSum);
+
+    // send bcd value to 7-seg port
+    *(bcdPortPtr  + PIO_DATA_REG_OFFSET) = bcd_value;
+  } /* while */
+
+  return (0);
+} /* main */
+
---
+++ cpet561/lab2/software/nios_software/obj/default/C_accumulator_main.d
@@ -0,0 +1,6 @@
+obj/default/C_accumulator_main.o: C_accumulator_main.c \
+ ../nios_software_bsp/system.h ../nios_software_bsp/linker.h
+
+../nios_software_bsp/system.h:
+
+../nios_software_bsp/linker.h:
