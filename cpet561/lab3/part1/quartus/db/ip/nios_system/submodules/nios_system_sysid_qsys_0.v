7e454174ecb6349510bd85eb3f510daacd2657a7
Zachary Weeden (RIT Student) <zdw7287@ad.rit.edu>
2017-02-20 14:12:42 -0500

    Recompile

--- cpet561/lab3/part1/quartus/db/ip/nios_system/submodules/nios_system_sysid_qsys_0.v
+++ cpet561/lab3/part1/quartus/db/ip/nios_system/submodules/nios_system_sysid_qsys_0.v
@@ -49,7 +49,7 @@ module nios_system_sysid_qsys_0 (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1487193546 : 3;
+  assign readdata = address ? 1487617751 : 3;
 
 endmodule
 
