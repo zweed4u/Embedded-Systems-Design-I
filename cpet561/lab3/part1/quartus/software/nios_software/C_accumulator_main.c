ca873bc046b3769d1286a12a96d4cbb69341027a
Zack <zackweeden@aol.com>
2017-02-20 23:40:26 -0500

    Fix spacing

--- cpet561/lab3/part1/quartus/software/nios_software/C_accumulator_main.c
+++ cpet561/lab3/part1/quartus/software/nios_software/C_accumulator_main.c
@@ -10,45 +10,45 @@
 #include <stdlib.h>
 #include <math.h>
 #define numBytes 5
-unsigned long *ramBase_ptr = (unsigned long *) INFERRED_RAM_BASE;	// Pointer to the base address of Inferred RAM
-unsigned char *ledBase_ptr = (unsigned char *) LEDS_BASE;			// Pointer to LED base memory location
-unsigned char *triggerBase_ptr = (unsigned char *) KEY1_BASE;		// Pointer to Key1 base memory location
-int randomNum = 0;													// Global pattern variable
-int randomStore[numBytes];											// Array for storing the random pattern
+unsigned long *ramBase_ptr = (unsigned long *) INFERRED_RAM_BASE;   // Pointer to the base address of Inferred RAM
+unsigned char *ledBase_ptr = (unsigned char *) LEDS_BASE;           // Pointer to LED base memory location
+unsigned char *triggerBase_ptr = (unsigned char *) KEY1_BASE;       // Pointer to Key1 base memory location
+int randomNum = 0;                                                  // Global pattern variable
+int randomStore[numBytes];                                          // Array for storing the random pattern
 
 /* Returns a random number using the epoch as seed. */
 int getRandomPattern(void){
-	randomNum = rand();
-	randomNum = (randomNum%100);
-	return (randomNum); //Store each in array to check after
+    randomNum = rand();
+    randomNum = (randomNum%100);
+    return (randomNum); //Store each in array to check after
 }
 /* Return 0 or 1, for False or True respectively, if memory passes test.
  * Params: ramLocation_ptr - location of the RAM wanted to check.
  * numBytesToCheck - Size in bytes of RAM wanted to check. */
 int ramConfidenceTest(unsigned long *ramLocation_ptr, unsigned int numBytesToCheck){
-	//verify the write to ram here
-	return 1; //Pass for now
+    //verify the write to ram here
+    return 1; //Pass for now
 }
 /* Write random values ram
  * Params: ramLocation_ptr - location of the RAM wanted to check.
  * numBytesToCheck - Size in bytes of RAM wanted to check. */
 void writeToRAM(unsigned long *ramLocation_ptr, unsigned int numBytesToCheck){
-	int i = 0;
-	while (numBytesToCheck){ //While we still need to write more bytes
-		getRandomPattern();
-		randomStore[i] = randomNum;
-		*(ramLocation_ptr + i) = randomNum; // +4 columns
-		i++; //increment counter for mem address from base and array of pattern
-		numBytesToCheck--; //Decrement bytes for how many more to write to
-	}
+    int i = 0;
+    while (numBytesToCheck){ //While we still need to write more bytes
+        getRandomPattern();
+        randomStore[i] = randomNum;
+        *(ramLocation_ptr + i) = randomNum; // +4 columns
+        i++; //increment counter for mem address from base and array of pattern
+        numBytesToCheck--; //Decrement bytes for how many more to write to
+    }
 }
 
 int main(void)
 {
-	int pass = 0; //initialize passing variable to failure
-	srand((unsigned)time(NULL)); // Fetch current epoch and use as seed
-	writeToRAM(ramBase_ptr, numByte); //Write values to RAM
-	//pass = ramCon...
-	ramConfidenceTest(ramBase_ptr, numBytes); //Verify correct values written
-	return(0);
+    int pass = 0; //initialize passing variable to failure
+    srand((unsigned)time(NULL)); // Fetch current epoch and use as seed
+    writeToRAM(ramBase_ptr, numByte); //Write values to RAM
+    //pass = ramCon...
+    ramConfidenceTest(ramBase_ptr, numBytes); //Verify correct values written
+    return(0);
 }
--- cpet561/lab3/part1/src/lab3.vhd
+++ cpet561/lab3/part1/src/lab3.vhd
@@ -96,11 +96,11 @@ architecture lab3_arch of lab3 is
   
   component nios_system is
     port (
-		clk_clk       : in  std_logic                    := 'X'; -- clk
-		reset_reset_n : in  std_logic                    := 'X'; -- reset_n
-		leds_export   : out std_logic_vector(7 downto 0);        -- export
-		key1_export   : in  std_logic                    := 'X'  -- export
-	 );
+    clk_clk       : in  std_logic                    := 'X'; -- clk
+    reset_reset_n : in  std_logic                    := 'X'; -- reset_n
+    leds_export   : out std_logic_vector(7 downto 0);        -- export
+    key1_export   : in  std_logic                    := 'X'  -- export
+   );
   end component nios_system;
   
 begin
@@ -129,11 +129,11 @@ begin
     
   ----- Instantiate the nios processor
   nios_unit : component nios_system
-	 port map (
-      clk_clk       => CLOCK2_50,			--   clk.clk
-		reset_reset_n => reset_n, 				-- reset.reset_n
-		leds_export   => LEDR(7 downto 0),	--  leds.export
-		key1_export    => accumPulse			--   key.export
-	 );
-	 
+   port map (
+      clk_clk       => CLOCK2_50,     --   clk.clk
+    reset_reset_n => reset_n,         -- reset.reset_n
+    leds_export   => LEDR(7 downto 0),  --  leds.export
+    key1_export    => accumPulse      --   key.export
+   );
+   
 end architecture lab3_arch;
