eff240f9317f33d5e311f212f1916d75c46d4ecd
Zachary Weeden (RIT Student) <zdw7287@ad.rit.edu>
2017-04-05 16:06:23 -0400

    Removing unpacked lab4 folder

--- lab4/lab4_quartus/software/nios_software/obj/default/lab4_main.d
+++
@@ -1,8 +0,0 @@
-obj/default/lab4_main.o: lab4_main.c ../nios_software_bsp/system.h \
- ../nios_software_bsp/linker.h ../nios_software_bsp//HAL/inc/alt_types.h
-
-../nios_software_bsp/system.h:
-
-../nios_software_bsp/linker.h:
-
-../nios_software_bsp//HAL/inc/alt_types.h:
